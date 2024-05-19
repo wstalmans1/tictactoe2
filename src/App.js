7af3c565d2d8863c09c1bcc44d1311a97979ad66
Wim Stalmans <wim.stalmans@gmail.com>
2024-05-19 22:26:59 +0200

    handleClick

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function Square({ value }){
-  return <button className="square">{value}</button>;
+  function handleClick(){
+    console.log("clicked");
+  }
+  return (
+    <button className="square" onClick={handleClick}>{value}</button>
+  )
 }
 
 
